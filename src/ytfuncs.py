1512469b87e715c1d425b2ba1a72d976d01d5f8f
Anson <tmmanson77@gmail.com>
2023-09-24 22:04:45 +0100

    fix: missing ytb object in function; better error handling

--- src/ytfuncs.py
+++ src/ytfuncs.py
@@ -4,79 +4,100 @@ import sys
 from time import sleep
 from config.config import ytfile
 
+
 def ytmusic_auth():
     # Attempt to authenticate Youtube music
     try:
         ytmusic = YTMusic(ytfile)
-        message("y+","Successfully Authenticated")
+        message("y+", "Successfully Authenticated")
         return ytmusic
     except:
-        message("y+","Authentication failed")
+        message("y+", "Authentication failed")
         sys.exit(0)
-      
+
+
 def get_youtube_playlists(ytmusic):
-   # Gets user youtube music playlists
-   user_playlists = ytmusic.get_library_playlists(1000)
-   yt_lists = {}
-   for i in user_playlists:
-      playlist_name = i['title']
-      playlist_id = i['playlistId']
-      yt_lists[playlist_name] = playlist_id
-   return yt_lists
+    # Gets user youtube music playlists
+    user_playlists = ytmusic.get_library_playlists(1000)
+    yt_lists = {}
+    for i in user_playlists:
+        playlist_name = i['title']
+        playlist_id = i['playlistId']
+        yt_lists[playlist_name] = playlist_id
+    return yt_lists
+
 
 def change_name(ytmusic, yt_lists):
-   # Changes spfy2yt to sound-tunnel for users of old script
-   for i in yt_lists:
-      if "spfy2yt" in i:
-         new_name = i.replace("spfy2yt","sound-tunnel")
-         id = yt_lists[i]
-         success = ytmusic.edit_playlist(id,new_name)
-         if success == 'STATUS_SUCCEEDED':
-            message("y+","Renamed {} to {} to fit new script".format(i,new_name))
-            
+    # Changes spfy2yt to sound-tunnel for users of old script
+    for i in yt_lists:
+        if "spfy2yt" in i:
+            new_name = i.replace("spfy2yt", "sound-tunnel")
+            id = yt_lists[i]
+            success = ytmusic.edit_playlist(id, new_name)
+            if success == 'STATUS_SUCCEEDED':
+                message(
+                    "y+", "Renamed {} to {} to fit new script".format(i, new_name))
+
+
 def get_yt_playlist_content(ytmusic, source_id):
-   playlist_content = ytmusic.get_playlist(source_id)
-   result = []
-   for song in playlist_content['tracks']:
-      song_name = song['title']
-      try:
-         album_name = song['album']['name']
-      except:
-         album_name = ""
-      artist_name = []
-      for i in song['artists']:
-         artist = i['name']
-         artist_name.append(artist)
-      artist = ' '.join(artist_name)
-      result.append(album_name+"&"+song_name+"&"+artist)
-   return result
+    playlist_content = ytmusic.get_playlist(source_id)
+    result = []
+    for song in playlist_content['tracks']:
+        song_name = song['title']
+        try:
+            album_name = song['album']['name']
+        except:
+            album_name = ""
+        artist_name = []
+        for i in song['artists']:
+            artist = i['name']
+            artist_name.append(artist)
+        artist = ' '.join(artist_name)
+        result.append(album_name+"&"+song_name+"&"+artist)
+    return result
+
 
 def yt_dest_check(ytmusic, yt_lists, dest_playlist_name):
-   if dest_playlist_name in yt_lists:
-      dest_playlist_id = yt_lists[dest_playlist_name]
-      message("y+","Playlist exists, adding missing songs")
-   else:
-      dest_playlist_id = ytmusic.create_playlist(dest_playlist_name,"Sound Tunnel playlist")
-      message("y+", "Playlist created")
-   return dest_playlist_id
+    if dest_playlist_name in yt_lists:
+        dest_playlist_id = yt_lists[dest_playlist_name]
+        message("y+", "Playlist exists, adding missing songs")
+    else:
+        dest_playlist_id = ytmusic.create_playlist(
+            dest_playlist_name, "Sound Tunnel playlist")
+        message("y+", "Playlist created")
+    return dest_playlist_id
+
 
 def move_to_ytmusic(ytmusic, playlist_info, dest_id):
-   not_found = []
-   present_song = get_yt_playlist_content(dest_id)
-   playlist_info = what_to_move(present_song, playlist_info)
-   not_found = []
-   try:
-      for i in playlist_info:
-         i = i.replace("&", " ")
-         search = ytmusic.search(i, "songs")
-         songid = [search[0]['videoId']]
-         sleep(0.5)
-         add_success = ytmusic.add_playlist_items(dest_id, songid)
-         # Didn't add compare since yt has everything and has good search algo
-         if "song is already in the playlist" in add_success or "SUCCEEDED" in add_success:
-            pass
-         else:
-            not_found.append(i)
-      return not_found
-   except Exception:
-      return not_found
+    not_found = []
+    present_song = get_yt_playlist_content(ytmusic, dest_id)
+    playlist_info = what_to_move(present_song, playlist_info)
+    print(f'moving below!: number of songs:{len(playlist_info)}')
+   #  print(playlist_info)
+
+    not_found = []
+    flag = 1
+    tmp = 0
+    while flag:
+        try:
+            for idx in range(tmp, len(playlist_info)):
+                i = playlist_info[idx]
+                tmp = idx+1
+                if i == playlist_info[-1]:
+                    flag = 0
+                i = i.replace("&", " ")
+                i = i.replace('@#72', ' ')
+                search = ytmusic.search(i, "songs")
+                songid = [search[0]['videoId']]
+                sleep(1)
+                add_success = ytmusic.add_playlist_items(dest_id, songid)
+                # Didn't add compare since yt has everything and has good search algo
+                if "song is already in the playlist" in add_success or "SUCCEEDED" in add_success:
+                    print(f"already in playlist:{i}")
+                    pass
+                else:
+                    not_found.append(i)
+        except Exception:
+            print(f"not found/search error:{i}")
+            # return not_found
+    return not_found
